f741c7884c8434141e38193203c3b7324a693adb
James Michael Monroe <james@scvhometownweb.com>
2020-03-17 16:31:12 -0700

    Updates

---
+++ precache-manifest.657083c1de69ba8c7f7d5226f157eb1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a6da9f47cf86111af661d57e8dc30e",
+    "url": "/kanitsunami/index.html"
+  },
+  {
+    "revision": "6513430db599e73c7a4c",
+    "url": "/kanitsunami/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "72554ab2689e0d223ec3",
+    "url": "/kanitsunami/static/js/2.00a0a7fd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/kanitsunami/static/js/2.00a0a7fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6513430db599e73c7a4c",
+    "url": "/kanitsunami/static/js/main.1dab3d19.chunk.js"
+  },
+  {
+    "revision": "3fcfc3e5512fa2f51916",
+    "url": "/kanitsunami/static/js/runtime-main.81d0ee97.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kanitsunami/static/media/logo.5d5d9eef.svg"
+  }
+]);
